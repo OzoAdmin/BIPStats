d0efd495c7b0ab94384e1a79d50df1ed35310bf0
OzoAdmin <ozoadmin@gmail.com>
2017-03-02 22:03:03 +0100

    Przygotowanie do pobierania menu i podmemu

--- test.py
+++ test.py
@@ -3,51 +3,57 @@ from bs4 import BeautifulSoup
 #import requests
 import re
 
-url = 'http://bip.parzeczew.nv.pl/Article/get/id,15320.html'
+url_to_get = ['http://bip.parzeczew.nv.pl/Article/get/id,15344.html', 'http://bip.parzeczew.nv.pl/Article/get/id,15341.html']
 #fhand = urllib.urlopen('http://bip.parzeczew.nv.pl')
 
-html = urllib.urlopen(url).read()
+def get_url_to_get(url):
 
-soup = BeautifulSoup(html, "html.parser")
 
-#print soup.find_all("a", class_="button-download")
-pliki = soup.find_all("div", class_="line-row")
+def get_uchwaly(url):
 
-for item in pliki:
+    html = urllib.urlopen(url).read()
+    soup = BeautifulSoup(html, "html.parser")
 
-    #print item
-    print '-------------------------'
+    #print soup.find_all("a", class_="button-download")
+    pliki = soup.find_all("div", class_="line-row")
 
-    text = item.find("p").get_text()
-    text = text.encode('utf-8')
-    print text
+    for item in pliki:
 
-    attrib = item.find_all("span")
-    #format = text.encode('utf-8')
-    file_size = attrib[0].get_text()
-    file_date_add = attrib[1].get_text()
-    file_format = attrib[2].get_text()
-    print file_size
-    print file_date_add
-    print file_format
+        #print item
+        print '-------------------------'
 
-    a = item.find("a").get('href')
-    a = a.encode('utf-8')
-    #href = a["href"]
-    print a
+        text = item.find("p").get_text()
+        text = text.encode('utf-8')
+        print text
 
-print '______________________________'
-print 'Pobrano pozycji: ', len(pliki)
-print '______________________________'
-#print(dd.pretiffy())
+        attrib = item.find_all("span")
+        #format = text.encode('utf-8')
+        file_size = attrib[0].get_text()
+        file_date_add = attrib[1].get_text()
+        file_format = attrib[2].get_text()
+        print file_size
+        print file_date_add
+        print file_format
 
+        a = item.find("a").get('href')
+        a = a.encode('utf-8')
+        #href = a["href"]
+        print a
+
+    print '______________________________'
+    print 'Pobrano pozycji: ', len(pliki)
+    print '______________________________'
+    #print(dd.pretiffy())
+
+for url in url_to_get:
+    get_uchwaly(url)
 
 #print soup.select("div.line-row p")
 #print soup.select("a.button-download")
-for tag in soup.find_all("div", class_="line-row"):
+#for tag in soup.find_all("div", class_="line-row"):
     #print tag.attrs
     #type(tag)
-    '''file_to_get = tag.get('href')
+'''file_to_get = tag.get('href')
     print tag.get('href')
     get_filename = re.findall('([0-9]*).html', file_to_get)
     filename = get_filename[0]
@@ -57,8 +63,7 @@ for tag in soup.find_all("div", class_="line-row"):
     #urllib.urlretrieve(file_to_get, 'files/'+filename+'.pdf')
     print 'Plik files/'+filename+'.pdf zostal zapisany'
 
-'''
-    '''img = urllib.urlopen(file_to_get)
+img = urllib.urlopen(file_to_get)
     fhand = open(filename+'.pdf', 'w')
     size = 0
     while True:
@@ -67,7 +72,8 @@ for tag in soup.find_all("div", class_="line-row"):
         size = size + len(info)
         fhand.write(info)
     print size,'characters copied.'
-    fhand.close()'''
+    fhand.close()
+
 
 
 # Retrieve all of the anchor tags
@@ -78,3 +84,4 @@ for tag in soup.find_all("div", class_="line-row"):
     #print tag.get('class', None)
 
 #soup.find_all("a", "menu")
+'''
